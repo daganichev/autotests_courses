472d1bec5ddfa9fd012f11a2e8f357ba6d81de77
daganichev <da.ganichev@tensor.ru>
2023-06-08 23:30:19 +0300

    Заданий к вебинару 10 Дополнение

--- Lesson10/conftest.py
+++ Lesson10/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import datetime
 
 
-@pytest.fixture
+@pytest.fixture(scope='class')
 def start_end_time_log():
     start_time = datetime.datetime.now()
     print(f"\nВремя начала тестов: {start_time.strftime('%H:%M:%S')}")
@@ -16,5 +16,5 @@ def time_log():
     start_time = datetime.datetime.now()
     yield
     end_time = datetime.datetime.now()
-    exec_time = end_time - start_time
-    return print(f"\nВремя выполнения теста: {exec_time} сек.")
+    difference_time = end_time - start_time
+    return print(f"\nВремя выполнения теста: {difference_time} сек.")
